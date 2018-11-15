00a1c3d4a3686c74b74661012bfb814217452253
grandee25 <grandee25@naver.com>
2018-11-15 18:07:20 +0900

    tf file modified final

--- aws.tf
+++ aws.tf
@@ -8,8 +8,24 @@ resource "aws_instance" "example" {
   ami           = "ami-0f65671a86f061fcd"
   instance_type = "t2.micro"
   key_name = "infra-ci-cookbooks"
+  vpc_security_group_ids = ["${aws_security_group.test.id}"]
 }
 
-#resource "aws_eip" "ip" {
-#  instance = "${aws_instance.example.id}"
-#}
+resource "aws_eip" "ip" {
+  instance = "${aws_instance.example.id}"
+}
+
+resource "aws_security_group" "test" {
+  name = "test"
+  description = "test"
+  vpc_id = "vpc-4ea39926"
+}
+
+resource "aws_security_group_rule" "for_test" {
+  type = "ingress"
+  from_port = 22
+  to_port = 22
+  protocol = "TCP"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.test.id}"
+}
